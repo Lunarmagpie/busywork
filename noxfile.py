e1df41e47f638fbcb4a3c694bda197ecd207ae9e
Lunarmagpie <bambolambo0@gmail.com>
2022-12-01 00:00:23 -0500

    update session

--- noxfile.py
+++ noxfile.py
@@ -1,21 +1,32 @@
 import nox
 
 
-@nox.session
+def session(*groups: str):
+    def inner(func):
+        @nox.session(name=func.__name__)
+        def session(session: nox.Session):
+            session.install("busywork")
+            for group in groups:
+                session.run("busywork", "install", "-g", group)
+            func(session)
+
+        return session
+
+    return inner
+
+
+@session("typing")
 def mypy(session: nox.Session) -> None:
-    session.run("busywork", "install", "-g", "typing")
     session.run("mypy", "busywork")
 
 
-@nox.session(name="format")
+@session("linting")
 def format(session: nox.Session) -> None:
-    session.run("busywork", "install", "-g", "linting")
     session.run("black", ".")
     session.run("isort", ".")
 
 
-@nox.session
+@session("linting")
 def lint(session: nox.Session) -> None:
-    session.run("busywork", "install", "-g", "linting")
     session.run("black", ".", "--check")
     session.run("isort", ".", "--check")
